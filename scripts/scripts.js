cc3a3a72379e890b9796823ad5294e2248694e36
James Stephens <jaystephens3.0@gmail.com>
2020-03-18 08:27:32 -0400

    Update2
    started working towards getting the site cleaned up

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,48 @@
-$( document ).ready(function() {
-    //Set header to fixed postion when scrolled to, and adjust container below
-    $(".arrowbtn-down").click(function() {
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $("#header").offset().top
-        }, 2000);
-    });
-
-    //Set header to fixed postion when scrolled to, and adjust container below
-    var waypoint = new Waypoint({
-        element: document.getElementById('header'),
-        handler: function() {
-            $("#header").toggleClass("fixHeader");
-            if ($("#header").hasClass("fixHeader")) {
-                $(".about").addClass("fixAbout");
-                // $(".landing").addClass("hideLanding");
-            }
-            else {
-                $(".about").removeClass("fixAbout");
-            }
-        }
-    });
-});
-
+$( document ).ready(function() {
+
+    //Set header to fixed postion when scrolled to, and adjust container below
+
+    $(".arrowbtn-down").click(function() {
+
+        $([document.documentElement, document.body]).animate({
+
+            scrollTop: $("#about").offset().top
+
+        }, 2000);
+
+    });
+
+
+
+    //Set header to fixed postion when scrolled to, and adjust container below
+
+    var waypoint = new Waypoint({
+
+        element: document.getElementById('header'),
+
+        handler: function() {
+
+            $("#header").toggleClass("fixHeader");
+
+            if ($("#header").hasClass("fixHeader")) {
+
+                $(".about").addClass("fixAbout");
+
+                // $(".landing").addClass("hideLanding");
+
+            }
+
+            else {
+
+                $(".about").removeClass("fixAbout");
+
+            }
+
+        }
+
+    });
+
+});
+
+
+
